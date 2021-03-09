cbc100e09e608584b57d2503f77b1a548737d11d
Nick Smith <nick.smith@cern.ch>
2021-03-08 21:58:47 -0600

    It works

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import Autocomplete from "./Autocomplete";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="App Title">
+        Fuzzy Dataset Search
       </header>
+      <div className="App SearchBar">
+        <div className="Sep">/</div>
+        <Autocomplete name="primary" options={["GluGluSpin0ToGammaGamma_W_1p4_M_750_TuneCP2_13TeV_pythia8","VBFToRadionToHHTo4B_M-850_narrow_13TeV-madgraph","VBF_LFV_HToMuTau_M150_13TeV_powheg_pythia8","GluGluSpin0ToGammaGamma_W_5p6_M_3000_TuneCP2_13TeV_pythia8","HWminusJ_HToWW_M120_13TeV_powheg_pythia8_TuneCP5","PythiaChargedResonance_WGToJJG_M2400_width5","PythiaChargedResonance_WGToLNuG_M250_width0p01","BprimeTToHB_M-1400_RH_TuneCUETP8M1_13TeV-madgraph-pythia8","ZprimeToZhToZhadhbb_narrow_M-3500_13TeV-madgraph","gluinoGMSB_M2900_ctau1000p0_TuneCP2_13TeV_pythia8"]} />
+        <div className="Sep">/</div>
+        <Autocomplete name="conditions" options={["RunIIFall17NanoAODv6-PU2017_12Apr2018_Nano25Oct2019_102X_mc2017_realistic_v7-v1","RunIIFall17NanoAODv6-PU2017_12Apr2018_Nano25Oct2019_102X_mc2017_realistic_v7-v2","RunIISummer16NanoAODv6-PUMoriond17_Nano25Oct2019_102X_mcRun2_asymptotic_v7-v1","RunIIFall17NanoAODv6-PU2017_12Apr2018_Nano25Oct2019_tauDecays_102X_mc2017_realistic_v7-v1","RunIISummer16NanoAODv6-PUMoriond17_Nano25Oct2019_102X_mcRun2_asymptotic_v7_ext1-v1","RunIIAutumn18NanoAODv6-PUFall18Fast_Nano25Oct2019_102X_upgrade2018_realistic_v20-v1","RunIIFall17NanoAODv6-PU2017_12Apr2018_Nano25Oct2019_102X_mc2017_realistic_v7_ext1-v1","RunIIFall17NanoAODv6-PUFall17Fast_Nano25Oct2019_102X_mc2017_realistic_v7-v1","RunIIAutumn18NanoAODv4-Nano14Dec2018_102X_upgrade2018_realistic_v16-v1","RunIIFall17NanoAODv6-PU2017_12Apr2018_Nano25Oct2019_new_pmx_102X_mc2017_realistic_v7-v2"]} />
+        <div className="Sep">/</div>
+        <Autocomplete name="datatier" options={["NANOAOD", "NANOAODSIM", "MINIAOD"]} default="NANOAODSIM" />
+      </div>
     </div>
   );
 }
---
+++ src/Autocomplete.js
@@ -0,0 +1,140 @@
+import React, { Component, Fragment } from "react";
+import PropTypes from "prop-types";
+import fuzzysearch from "fuzzysearch";
+import './Autocomplete.css';
+ 
+class Autocomplete extends Component {
+  static propTypes = {
+    options: PropTypes.instanceOf(Array),
+    default: PropTypes.string,
+    name: PropTypes.string
+  };
+  
+  static defaultProps = {
+    options: [],
+    default: "",
+    name: "menu"
+  };
+  
+  constructor(props) {
+    super(props);
+  
+    this.state = {
+      activeOption: 0,
+      filteredOptions: this.props.options,
+      showOptions: false,
+      userInput: this.props.default
+    };
+    this.blurTimeout = null;
+
+    this.refreshOptions = this.refreshOptions.bind(this);
+    this.handleTextChange = this.handleTextChange.bind(this);
+    this.handleListClick = this.handleListClick.bind(this);
+    this.handleKeyPress = this.handleKeyPress.bind(this);
+    this.handleDivBlur = this.handleDivBlur.bind(this);
+    this.handleDivFocus = this.handleDivFocus.bind(this);
+  }
+  
+  refreshOptions(userInput) {
+    const filteredOptions = this.props.options.filter(
+      (option) => fuzzysearch(userInput, option)
+    );
+    this.setState({
+      activeOption: 0,
+      filteredOptions,
+      showOptions: true,
+      userInput: userInput
+    });
+  };
+
+  handleTextChange(e) {
+    this.refreshOptions(e.currentTarget.value);
+  };
+  
+  handleListClick(e) {
+    console.log("a click");
+    this.refreshOptions(e.currentTarget.innerText);
+  };
+  
+  handleKeyPress(e) {
+    if (e.code === "Enter") {
+      this.setState({
+        activeOption: 0,
+        showOptions: false,
+        userInput: this.state.filteredOptions[this.state.activeOption]
+      });
+    } else if (e.code === "ArrowUp") {
+      this.setState({ activeOption: (this.state.activeOption + this.state.filteredOptions.length - 1) % this.state.filteredOptions.length });
+    } else if (e.code === "ArrowDown") {
+      this.setState({ activeOption: (this.state.activeOption + 1) % this.state.filteredOptions.length });
+    }
+  };
+  
+  handleDivBlur(e) {
+    console.log("a blur");
+    this.blurTimeout = setTimeout(() => {
+      console.log("doing a blur");
+      this.setState({
+        showOptions: false
+      });
+    }, 500);
+  };
+
+  handleDivFocus(e) {
+    console.log("a focus");
+    clearTimeout(this.blurTimeout);
+    this.setState({
+      showOptions: true
+    });
+  };
+
+  render() {
+    console.log("render");
+    let optionsListComponent;
+  
+    if (this.state.showOptions) {
+      if (this.state.filteredOptions.length) {
+        optionsListComponent = (
+          <ul className="Autocomplete-options" key={this.name}>
+            {this.state.filteredOptions.map((option, index) => {
+              let className = "option";
+  
+              if (index === this.state.activeOption) {
+                className = "option active";
+              }
+  
+              return (
+                <li className={className} key={option} onClick={this.handleListClick}>
+                  {option}
+                </li>
+              );
+            })}
+          </ul>
+        );
+      } else {
+        optionsListComponent = (
+          <ul className="Autocomplete-options">
+            <li className="option"><i>No options</i></li>
+          </ul>
+        );
+      }
+    }
+  
+    return (
+      <Fragment>
+        <div className="Autocomplete-box" key={this.name} onBlur={this.handleDivBlur} onFocus={this.handleDivFocus}>
+            <input
+              type="text"
+              onChange={this.handleTextChange}
+              onKeyDown={this.handleKeyPress}
+              value={this.state.userInput}
+              className="Autocomplete-input"
+            />
+            {optionsListComponent}
+        </div>
+      </Fragment>
+    );
+  }
+  }
+  
+  export default Autocomplete;
